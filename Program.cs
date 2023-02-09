17cca315b39895db89fbe8fb916f1d04f86dbaf5
William Rocha <uill_pf@hotmail.com>
2023-02-09 13:08:13 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//EXERCÌCIO 2
+
+//Armazenamento de dados
+
+double baseT;
+double altura;
+double resultado;
+
+//Entrada de dados
+
+Console.WriteLine("");
+Console.WriteLine("Olá. Digite o valor da base do seu triangulo");
+baseT = double.Parse(Console.ReadLine());
+Console.WriteLine("Certo, agora digite o valor da altura do triangulo");
+altura = double.Parse(Console.ReadLine());
+
+//Opração a ser realizada
+
+resultado = (baseT*altura)/2;
+
+//Resultado
+
+Console.WriteLine("");
+Console.WriteLine("O resultado da Área do Triangulo é " + resultado);
